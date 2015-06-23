321b1982caada7fce8bb20ba8160dd7046ada421
andygout <andygout@hotmail.co.uk>
2015-06-23 09:24:29 +0100

    Adds: Full reset functionality; YEAR QUIZ READY

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,10 +12,18 @@ function submitAnswer() {
   var yearAnswer = document.getElementById('yearAnswer').innerText;
   var yearGuess = document.getElementById('yearGuess').value;
   if(yearGuess == yearAnswer) {
-    document.getElementById('result').innerHTML = 'Correct: -1 points';
+    document.getElementById('result').innerHTML = 'Correct: +5 points';
   } else {
     document.getElementById('result').innerHTML = 'Wrong: -1 points';
   }
   document.getElementById('answer').innerHTML = 'Released in ' + yearAnswer;
   $("#reset").show();
+}
+
+function newFilm() {
+  displayFilm(selectFilm());
+  $("#reset").hide();
+  document.getElementById('result').innerHTML = '';
+  document.getElementById('answer').innerHTML = '';
+  document.getElementById('yearGuess').value = '';
 }
